a802cffd0663af8eb1816d88beefdf17db50c6f1
op111111OP <p47601758@gmail.com>
2024-01-31 16:50:57 +0200

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ["www.aswo.com", "doctor-h.com.ua", "lh3.googleusercontent.com"],
   },
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias["react"] = "react/node_modules/react";
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
