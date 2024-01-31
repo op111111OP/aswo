e8f18df4f9b3cc0d82811bb443d0982a427b8093
op111111OP <p47601758@gmail.com>
2024-01-31 16:37:47 +0200

    new file:   .eslintrc.json 	new file:   .gitignore    	new file:   .vscode/settings.json 	deleted:    README.md new file:   jsconfig.json 	new file:   next.config.js 	new file:   package-lock.json 	new file:   package.json    	new file:   public/next.svg    	new file:   public/vercel.svg
    	new file:   src/Context/store.js
    
    	new file:   src/app/Context/store.js
       	new file:   src/app/favicon.ico
    
    	new file:   src/app/globals.css
    
    	new file:   src/app/layout.js
    
    	new file:   src/app/not-found.js
    
    	new file:   src/app/page.js
    
    	new file:   src/app/page.module.css
    
    	new file:   src/components/Footer/Footer.js
    
    	new file:   src/components/Footer/Footer.module.css
    
    	new file:   src/components/Header/Header.js
    
    	new file:   src/components/Header/Header.module.css
    
    	new file:   src/components/Memu/Menu.js
    
    	new file:   src/components/Memu/Menu.module.css
    
    	new file:   src/components/Swiper/Swipe.js
    
    	new file:   src/components/Swiper/Swipe.module.css
    
    	new file:   src/components/SwiperBrand/SwiperBrand.js
    
    	new file:   src/components/SwiperBrand/SwiperBrand.module.css
    
    	new file:   src/components/SwiperNovelty/SwiperNovelty.js
    
    	new file:   src/components/SwiperNovelty/SwiperNovelty.module.css
    
    	new file:   src/pages/api/find.js
    
    	new file:   src/pages/api/findNovelty.js
    
    	new file:   src/pages/models/novelty.js
    
    	new file:   src/pages/models/orders.js

---
+++ src/app/layout.js
@@ -0,0 +1,36 @@
+import { Inter, Montserrat } from "next/font/google";
+import "./globals.css";
+import { GlobalContextProvider } from "./Context/store";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+const inter = Montserrat({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="https://www.aswo.com/typo3conf/ext/aswo/Resources/Public/Images/favicon.ico"
+          sizes="any"
+        />
+      </head>
+      <body className={inter.className}>
+        <div className="wraper">
+          <Header />
+
+          <GlobalContextProvider>
+            <section>{children}</section>
+          </GlobalContextProvider>
+
+          <Footer />
+        </div>
+      </body>
+    </html>
+  );
+}
