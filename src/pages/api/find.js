576f71fdd29e77a8d40a66145bd838660e4eb069
op111111OP <p47601758@gmail.com>
2024-02-01 13:02:29 +0200

    modified:   src/pages/api/find.js

--- src/pages/api/find.js
+++ src/pages/api/find.js
@@ -1,13 +1,10 @@
 import Orders from "./models/orders";
 import mongoose from "mongoose";
 
-mongoose.connect(
-  "mongodb+srv://p47601758:111111OPop@base.e0cgzgc.mongodb.net/menu?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const handleError = (res, err) => {
   console.error("Error:", err);
   res.status(500).json({ error: "Internal Server Error" });
