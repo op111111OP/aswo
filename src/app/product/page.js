07a782343924aae6ea5f65cf45047d68012f458d
op111111OP <p47601758@gmail.com>
2024-02-12 12:56:44 +0200

    modified:   src/app/product/page.js

--- src/app/product/page.js
+++ src/app/product/page.js
@@ -8,13 +8,16 @@ import { useEffect, useState } from "react";
 import { useUserContext } from "../Context/store";
 import { BsArrowLeft } from "react-icons/bs";
 import { useSearchParams } from "next/navigation";
+import Link from "next/link";
 
 export default function Page() {
   //   const { id, setUserId } = useUserContext();
   const searchParams = useSearchParams();
   const id = searchParams.get("id");
   const [flutters, setFlutters] = useState(null);
+  const [flut, setFlut] = useState(null);
   const [truF, setTruF] = useState(true);
+  const [name, setName] = useState("");
   //   const [idp, seIdp] = useState();
   useEffect(() => {
     const fetchData = async () => {
@@ -25,6 +28,7 @@ export default function Page() {
         );
         const data = await response.json();
         setFlutters(data);
+        setName(data.categori);
       } catch (error) {
         console.log("Что-то пошло не так...", error);
       } finally {
@@ -33,7 +37,21 @@ export default function Page() {
     };
     fetchData();
   }, [id]);
-
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`/api/oneproductrek/${name}`);
+        const data = await response.json();
+        setFlut(data);
+      } catch (error) {
+        console.log("Что-то пошло не так...", error);
+      } finally {
+        console.log("пошло так...");
+      }
+    };
+    fetchData();
+  }, [name]);
+  //   console.log(flut);
   return (
     <div className={styles.main}>
       {!truF && (
@@ -46,77 +64,162 @@ export default function Page() {
         />
       )}
       {flutters && (
-        <div className={styles.right_goods_box}>
-          {truF && <div className={styles.name}>{flutters.name}</div>}
-          <div className={styles.slide_box}>
-            <div className={styles.link_box}>
-              <div className={truF ? styles.img_box : styles.img_box_fals}>
-                <Image
-                  src={flutters.img}
-                  alt="Vercel Logo"
-                  sizes="(max-width: 768px) 50vw, (max-width: 1200px) 75vw, 100vw"
-                  className={styles.img}
-                  width={100}
-                  height={100}
-                  style={
-                    truF
-                      ? { height: "50vh", width: "70%", objectFit: "contain" }
-                      : {
-                          height: "400px",
-                          width: "50vw",
-                          objectFit: "contain",
-                        }
-                  }
-                  onClick={() => {
-                    setTruF((truF) => !truF);
-                  }}
-                />
+        <div className={styles.right_g_box}>
+          <div className={styles.right_goods_box}>
+            {truF && <div className={styles.name}>{flutters.name}</div>}
+            <div className={styles.slide_box}>
+              <div className={styles.link_box}>
+                <div className={truF ? styles.img_box : styles.img_box_fals}>
+                  <Image
+                    src={flutters.img}
+                    alt="Vercel Logo"
+                    sizes="(max-width: 768px) 50vw, (max-width: 1200px) 75vw, 100vw"
+                    className={styles.img}
+                    width={100}
+                    height={100}
+                    style={
+                      truF
+                        ? { height: "50vh", width: "70%", objectFit: "contain" }
+                        : {
+                            height: "400px",
+                            width: "50vw",
+                            objectFit: "contain",
+                          }
+                    }
+                    onClick={() => {
+                      setTruF((truF) => !truF);
+                    }}
+                  />
+                </div>
+                {truF && (
+                  <div className={styles.description_box}>
+                    <div className={styles.description_h1}>Опис</div>
+                    <div className={styles.description}>
+                      {flutters.description}
+                    </div>
+                  </div>
+                )}
               </div>
+
               {truF && (
-                <div className={styles.description_box}>
-                  <div className={styles.description_h1}>Опис</div>
-                  <div className={styles.description}>
-                    {flutters.description}
+                <div className={styles.price_box}>
+                  <div className={styles.price_box_pbox}>
+                    <div className={styles.price}>{flutters.price} грн.</div>
+                    <FaShoppingCart
+                      size={25}
+                      color=" #0058a2"
+                      onClick={() =>
+                        setUserId({
+                          categori: flutters.categori,
+                          brand: flutters.brand,
+                          country: flutters.country,
+                          description: flutters.description,
+                          img: flutters.img,
+                          name: flutters.name,
+                          price: flutters.price,
+                          id: flutters._id,
+                        })
+                      }
+                      className={styles.shopping}
+                    />
+                  </div>
+                  <div className={styles.characteristics_box}>
+                    <div className={styles.characteristics_h1}>
+                      Характеристики
+                    </div>
+                    <div className={styles.country_box}>
+                      <div className={styles.country_h1}>
+                        Країна виробництва:{" "}
+                      </div>
+                      <div className={styles.country}>{flutters.country}</div>
+                    </div>
                   </div>
                 </div>
               )}
             </div>
+          </div>
+          {/* //// */}
+          <div className={styles.right_goods_box2}>
+            {Array.isArray(flut) &&
+              flut.map((item, index) => (
+                <div
+                  className={styles.swiper_slide2}
+                  key={index}
+                  onMouseEnter={() => {
+                    setCehage(index);
+                  }}
+                  onMouseLeave={() => {
+                    setCehage(false);
+                  }}
+                >
+                  <div className={styles.slide_box2}>
+                    <Link href={`./product?id=${item._id2}`}>
+                      <div
+                        className={styles.link_box2}
+                        onClick={() => {
+                          setId(item._id);
+                        }}
+                      >
+                        <div className={styles.img_box2}>
+                          <Image
+                            className={styles.img2}
+                            src={item.img}
+                            alt="Vercel Logo"
+                            sizes="(max-width: 768px) 50vw, (max-width: 1200px) 75vw, 100vw"
+                            width={30}
+                            height={30}
+                            style={{
+                              height: "100%",
+                              width: "100%",
+                              objectFit: "contain",
+                            }}
+                          />
+                        </div>
+                        <div className={styles.text_box2}>{item.name}</div>
+                      </div>
+                    </Link>
+                    <div className={styles.price_box2}>
+                      {item.price === 0 && (
+                        <div
+                          className={styles.text_box_price2}
+                          style={{ fontSize: "10px", width: "77px" }}
+                        >
+                          Зверніться до адміністратора.
+                        </div>
+                      )}
+                      {item.price !== 0 && (
+                        <div className={styles.text_box_price2}>
+                          {item.price}грн
+                        </div>
+                      )}
 
-            {truF && (
-              <div className={styles.price_box}>
-                <div className={styles.price_box_pbox}>
-                  <div className={styles.price}>{flutters.price} грн.</div>
-                  <FaShoppingCart
-                    size={25}
-                    color=" #0058a2"
-                    onClick={() =>
-                      setUserId({
-                        categori: flutters.categori,
-                        brand: flutters.brand,
-                        country: flutters.country,
-                        description: flutters.description,
-                        img: flutters.img,
-                        name: flutters.name,
-                        price: flutters.price,
-                        id: flutters._id,
-                      })
-                    }
-                    className={styles.shopping}
-                  />
-                </div>
-                <div className={styles.characteristics_box}>
-                  <div className={styles.characteristics_h1}>
-                    Характеристики
-                  </div>
-                  <div className={styles.country_box}>
-                    <div className={styles.country_h1}>
-                      Країна виробництва:{" "}
+                      <FaShoppingCart
+                        size={25}
+                        color=" #0058a2"
+                        id="basket"
+                        onClick={(e) => {
+                          handleBasketClick(e, {
+                            categori: item.categori,
+                            brand: item.brand,
+                            country: item.country,
+                            description: item.description,
+                            img: item.img,
+                            name: item.name,
+                            price: item.price,
+                            id: item._id,
+                          });
+                        }}
+                        className={styles.shopping2}
+                      />
                     </div>
-                    <div className={styles.country}>{flutters.country}</div>
+
+                    <div className={styles.text_botom_box2}>
+                      <div>Бренд: {item.brand}</div>
+                      <div> Країна виробництва: {item.country}</div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            )}
+              ))}
           </div>
         </div>
       )}
